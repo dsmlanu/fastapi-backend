b27422e6ed5ac9921f4622b96f74f2d618d5fc90
dsmlanu <aanita.gomberr@gmail.com>
2025-12-27 19:24:27 +0530

    update update: switched to Groq API

--- main.py
+++ main.py
@@ -2,26 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import subprocess
 import json
+import os
+
+from app.rag_pipeline import call_llm
 
 app = FastAPI()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 @app.get("/")
 def home():
     return {"message": "FastAPI working!"}
 
-class LLMRequest(BaseModel):
+class Prompt(BaseModel):
     prompt: str
 
 @app.post("/generate")
-def generate(req: LLMRequest):
+def generate(data: Prompt):
     try:
-        result = subprocess.run(
-            ["ollama", "run", "llama3"],
-            input=req.prompt,
-            text=True,
-            capture_output=True,
-            encoding="utf-8"
-        )
-        return {"response": result.stdout}
+        response = call_llm(data.prompt)
+        return {"response": response}
     except Exception as e:
         return {"error": str(e)}
